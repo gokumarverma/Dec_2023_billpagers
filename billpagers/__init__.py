a8240670d0c64b5916993e6c9edd8e3d76634a12
Anil Kumar <go_kumaranil@yahoo.com>
2023-12-24 22:53:37 +0530

    Added custom error pages

--- billpagers/__init__.py
+++ billpagers/__init__.py
@@ -3,4 +3,6 @@ from flask import Flask
 app=Flask(__name__)
 
 from billpagers.core.views import core
+from billpagers.error_pages.handlers import error_pages
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
